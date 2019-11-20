cc6c59f4ac5db18ea5b87bb48fca9298e310e85e
asweingarten <asweingarten@gmail.com>
2019-11-20 02:02:17 -0800

    Animate strikethrough of value

--- src/App.svelte
+++ src/App.svelte
@@ -21,25 +21,42 @@
     }
   }
 
-  let scriptIndex = 0;
-  const linesOfDialouge = [
-    "How might we store product ranks in a relational database?",
-    "What happens when we insert product 24 into position 3?",
-    "Oh no :("
-  ];
-
   function noop() {
 
   }
 
-  function *script() {
-    // [prompt, data-transformation, acknowledgment/transition/inception]
-    // what if you had to speak the inception before you could click?
-    yield ["How might we store product ranks in a relational database", noop, "Put it in the data base!"];
-    yield ["What happens when we insert product 24 into position 3?", addRanks, "Constant time insertion"];
-    yield ["Oh no :(", insertRow(3, {id: 24, rank: 3}), "Oh no :("]; 
+  // the genericization of choreograph is v interesting.
+  // the data-type defines a dsl of transforms
+  function choreograph(filter, transform, interval) {
+    return () => {
+      const affected = products
+        .forEach((v, i) => {
+          if (filter(v)) {
+           products[i] = transform(products[i]); 
+          }
+        })
+    }
   }
 
+  function *script() {
+    // [before click, before click, what you click on]
+    // [after click, after click, what you are confronted with next]
+    // [prompt, data-transformation, acknowledgment/transition/inception]
+    // what if you had to speak the inception before you could click?
+    let line = "How might we store product ranks in a relational database";
+    yield [line, addRanks, "Put it in the data base!"];
+
+    line = "What happens when we insert product 24 into position 3?";
+    yield [line, insertRow(3, {id: 24, rank: 3}), "Constant time insertion"];
+
+    line = "Product 23 and 9 both have the same rank. Assume all products must have unique ranks.";
+    yield [line, noop, "What is the time complexity of fixing this?"];
+
+    line = "Restoring unique ids is a linear time operation under this approach.";
+    yield [ line
+          , choreograph((p) => p.id === 9, (p) => Object.assign(p, {isIncorrectValue: true}, 100))
+          , "Oh No :)"]
+  }
 
   let dialouge = "";
   let buttonText = "";
@@ -49,7 +66,11 @@
     if (sceneIsOver) { return }
 
     [dialouge, op, buttonText] = beat;
+  }
+
+  function executeOp() {
     op();
+    advanceScript();
   }
 
   const scene = script();
@@ -62,12 +83,11 @@
     <div class="row">
       <div class="col-xs-12 col-md-6 order-md-2 vertical-center">
         <p class="lead">{dialouge}</p>
-        <button on:click="{advanceScript}">{buttonText}</button>
+        <button on:click="{executeOp}">{buttonText}</button>
       </div>
 
       <div class="col-xs-12 col-md-6">
-        <h4 class="">Product Table</h4>
-        <Table bind:products></Table>
+        <Table bind:products name="Product"></Table>
       </div>
     </div>
   </div>
--- src/Table.svelte
+++ src/Table.svelte
@@ -3,8 +3,12 @@ import {flip} from 'svelte/animate';
 import {quintInOut} from 'svelte/easing';
 
 export let products = [];
+export let name = "";
 </script>
 
+{#if name !== ""}
+  <h4 class="">{name} Table</h4>
+{/if}
 <table style="margin: 0 auto; font-size: 2rem; text-align: right">
   <tbody>
     <tr>
@@ -18,6 +22,8 @@ export let products = [];
         <td>
           {#if p.rank}
             <span class="product-rank"
+                  class:wrong-value="{p.isIncorrectValue}"
+                  class:strike="{p.isIncorrectValue}"
             >{p.rank}</span>
           {/if}
         </td>
@@ -26,3 +32,37 @@ export let products = [];
 
   </tbody>
 </table>
+
+<style>
+/* .wrong-value {
+  text-decoration-color: red;
+  text-decoration-line: line-through;
+  transition: text-decoration-line 2s;
+} */
+
+@keyframes strike{
+  0%   { width : 0; }
+  100% { width: 100%; }
+}
+.strike {
+  position: relative;
+}
+.strike::after {
+  background: linear-gradient(45deg, rgba(255, 15, 15, 1), rgba(255, 0, 0, 1));
+  content: "";
+  height: 0.5rem;
+  left: 0;
+  margin-top: calc(0.125em / 2 * -1);
+  position: absolute;
+  right: 0;
+  top: 50%;
+  /* animation: 4s linear 1s strike; */
+  /* animation: 3s linear 1s 1 running strike; */
+  /* animation: 3s linear 1s 2 reverse both paused strike; */
+  animation-name: strike;
+  animation-duration: 0.5s;
+  animation-timing-function: ease-in;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;  
+}
+</style>
