dbc9c61c4520dfa9f93af434919a3b5d7a2dd05e
asweingarten <asweingarten@gmail.com>
2019-11-28 14:42:32 -0800

    Animate product ids and ranks separately

--- react-app/src/table/Table.js
+++ react-app/src/table/Table.js
@@ -1,22 +1,51 @@
 import React from 'react';
-import {useSpring, animated} from 'react-spring'
+import {useSpring, animated, useTransition} from 'react-spring'
 
 function ProductRow(props) {
-const spring = useSpring({opacity: 1, from: {opacity: 0}})
+// const spring = useSpring({opacity: 1, from: {opacity: 0}})
 return (
-  <animated.tr style={spring} className="product-row"
-      // animate:flip="{{easing: quadInOut}}"
+  <tr className="product-row"
   >
     <td className="product-id">{props.id}</td>
     <td>
-      <animated.span style={spring} className="product-rank"
-      >{props.rank}</animated.span>
+      <span  className="product-rank"
+      >{props.rank}</span>
     </td>
-  </animated.tr>
+  </tr>
   )
 };
 
 function Table(props) {
+  const ids = props.products.map(p => p.id);
+  console.log("ids")
+  console.log(ids)
+  const ranks = props.products.map(p => p.rank);
+  const idTransitions = useTransition(ids, item => `id:${item}`, {
+    from: { transform: 'translate3d(-40px,0px,0)', opacity: 0 },
+    enter: { transform: 'translate3d(0px,0px,0)', opacity: 1 },
+    leave: { transform: 'translate3d(-40px,0px,0)', opacity: 0 },
+  })
+  const rankTransitions = useTransition(ranks, item => `rank:${item}`, {
+    from: { transform: 'translate3d(40px,0px,0)', opacity: 0 },
+    enter: { transform: 'translate3d(0px,0px,0)', opacity: 1 },
+    leave: { transform: 'translate3d(40px,0px,0)', opacity: 0 },
+  })
+  const idNodes = idTransitions.map(({ item, props, key }) => {
+    return ( <animated.td style={props} className="product-id" key={key}>{item}</animated.td>)
+  });
+
+  const rankNodes = rankTransitions.map(({item, props, key}) => {
+    return ( <animated.td style={props} className="product-rank" key={key}>{item}</animated.td>)
+  });
+
+  const rows = props.products.map((p,i) => {
+    return (
+      <tr key={i} className="product-row">
+        {idNodes[i]}
+        {rankNodes[i]}
+      </tr>
+    )
+  })
   return (
     <div className="Table">
       <h4 className="text-center">{props.name || "Data"} Table</h4>
@@ -26,9 +55,7 @@ function Table(props) {
             <th>id</th>
             <th>rank</th>
           </tr>
-          {props.products.map(({id, rank}, i) => {
-            return <ProductRow id={id} rank={rank} key={i}></ProductRow>
-          })}
+          {rows}
         </tbody>
       </table>
     </div>
