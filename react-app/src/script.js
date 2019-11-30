f58ace30ffa8a30ebe2a7634d1f742647cbe6932
asweingarten <asweingarten@gmail.com>
2019-11-29 16:57:30 -0800

    Port the rest of the script

--- react-app/src/script.js
+++ react-app/src/script.js
@@ -39,4 +39,94 @@ export function *script() {
         , dialouge
         , buttonText 
         }
+
+
+  // beat 5
+  dialouge = "Restoring unique ids is a linear time operation under this approach.";
+  op = () => {}
+  buttonText = "Oh no :(";
+  yield { dialouge
+        , op
+        , buttonText
+      }
+
+
+  // beat 6
+  dialouge = "Ranking is a relative measurement. To determine the first item we can look at the lowest number. Can you think of a number lower than 1?";
+  op = (products, setProducts) => {
+    const updatedProducts = [{id: 77, rank: 0}, ...products];
+    setProducts(updatedProducts);
+  }
+  buttonText = "Oh yeah!";
+  yield { dialouge
+        , op
+        , buttonText
+      }
+        
+  // beat 7
+  // Can't generate the appropriate updateProducts function calls
+  //  until the insertRow is complete
+  // How might we chain the creation of the functions? can cues be a useful metaphor here?
+  dialouge = "However, this doesn't help if we insert Product 6 into position 3";
+  op = (products, setProducts) => {
+    const targetProducts = products.filter((p, i) => i >= 3).map(p => ({...p, rank: p.rank+1}));
+    const updatedProducts = [products[0], products[1], products[2], {id: 6, rank: 3}, ...targetProducts];
+
+    setProducts(updatedProducts);
+  }
+  buttonText = "Oh no :(";
+  yield { dialouge
+        , op
+        , buttonText
+        }
+  // beat 8
+  dialouge = "How might we achieve constant time insertion for any rank?"
+  op = () => {}
+  buttonText = "That's Impossible!"
+  yield { dialouge
+        , op
+        , buttonText
+        }
+  
+  // beat 9
+  dialouge = "What if we create some room?"
+  op = (products, setProducts) => {
+    const updatedProducts = products.map(p => ({...p, rank: p.rank*8}));
+    setProducts(updatedProducts);
+  }
+  buttonText = "I'm listening...";
+  yield { dialouge
+        , op
+        , buttonText
+        }
+  // beat 10
+  dialouge = "How many constant time insertions does this buy us per position?"
+  op = (products, setProducts) => {
+    const updatedProducts = [...products.slice(0,2)
+                            , {id: 4, rank:9}
+                            , {id: 3, rank:10}
+                            , {id: 2, rank:12}
+                            , ...products.slice(3)
+                            ];
+    setProducts(updatedProducts);
+  }
+          
+  buttonText = "log(8) = 3";
+  yield { dialouge
+        , op
+        , buttonText
+        }
+
+  // beat 11
+  dialouge = "And that's one way to amortize a linear time update"
+  op = (products, setProducts) => {
+    const updatedProducts = products.map((p,i) => ({...p, rank: (i)*1024}));
+    setProducts(updatedProducts);
+  }
+ 
+  buttonText = "Neat!";
+  yield { dialouge
+        , op
+        , buttonText
+        }
 }
